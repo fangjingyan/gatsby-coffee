61d26ba62c9f6f60453deb6d5b192421f46733b0
Jingyan Fang <jingyan727@gmail.com>
2019-10-02 22:12:31 -0400

    Connect to Contentful

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,12 +5,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
-const IndexPage = ({ data }) => (
+const AboutPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
-      title="regular joe's"
+      title="about us" styleClass="about-background"
     ></BackgroundSection>
     <Info />
   </Layout>
@@ -18,7 +18,7 @@ const IndexPage = ({ data }) => (
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "default-background.jpeg" }) {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -28,4 +28,4 @@ export const query = graphql`
   }
 `
 
-export default IndexPage
+export default AboutPage
